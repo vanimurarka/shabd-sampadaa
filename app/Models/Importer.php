a40d510092009853c2e9b524571f212cba163b32
Vani Murarka <vani.murarka@gmail.com>
2016-08-03 09:58:03 +0530

    done importing adverb file - but that is in db

--- app/Models/Importer.php
+++ app/Models/Importer.php
@@ -6,6 +6,8 @@ namespace App\Models;
 
 // use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Word;
+
 final class Importer
 {
 	// protected $table = 'ssp_words';
@@ -13,12 +15,14 @@ final class Importer
 
 	public static function importWords($wordFilename,$pos)
 	{
+		set_time_limit(60*5);
 		$file = fopen($wordFilename,"r");
 		while (!feof($file))
 		{
 			$line = fgets($file);
 			$wordEnd = strpos($line, " ");
-			echo substr($line,0,$wordEnd);
+			$word = substr($line,0,$wordEnd);
+			echo $word;
 			echo "<br/>";
 			$remainingLine = substr($line, $wordEnd);
 			echo $remainingLine."<br/>";
@@ -30,9 +34,16 @@ final class Importer
 			$synsetStart = 3+$numbers[1];
 			echo($synsetStart."<br/>");
 			for ($i=$synsetStart; $i < $synsetStart+$senseCount; $i++) { 
-				$synsetNumbersAsString .= $numbers[$i]." ";
+				if ($i > $synsetStart)
+					$synsetNumbersAsString .= ",";	
+				$synsetNumbersAsString .= (int)$numbers[$i];
 			}
 			echo($synsetNumbersAsString."<br/>");
+			$dbWord = new Word;
+			$dbWord->word = $word;
+			$dbWord->pos = $pos;
+			$dbWord->synsets = $synsetNumbersAsString;
+			$dbWord->save();
 		}
 		// echo fgets($file);
 		fclose($file);
